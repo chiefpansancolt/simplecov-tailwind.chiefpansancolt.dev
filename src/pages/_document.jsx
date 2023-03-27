ea5d148832a8e70e4234fd23dd55cff6508a0325
Christopher Pezza <chris.pezza@hey.com>
2023-03-26 21:32:14 -0400

    add tag

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -43,6 +43,20 @@ export default function Document() {
     <Html className="antialiased [font-feature-settings:'ss01']" lang="en">
       <Head>
         <script dangerouslySetInnerHTML={{ __html: themeScript }} />
+        <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+          `,
+            }}
+          />
       </Head>
       <body className="bg-white dark:bg-slate-900">
         <Main />
