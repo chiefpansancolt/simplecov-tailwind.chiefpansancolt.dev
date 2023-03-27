cd5bdccbe0d34877fe898352e6dd7a6007766197
Christopher Pezza <chris.pezza@hey.com>
2023-03-26 21:49:36 -0400

    add new favicon

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -59,6 +59,11 @@ export default function App({ Component, pageProps }) {
       <Head>
         <title>{pageTitle}</title>
         {description && <meta name="description" content={description} />}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <Layout title={title} tableOfContents={tableOfContents}>
         <Component {...pageProps} />
