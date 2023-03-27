5f9ddac5d44eb799f82d7f984e6324525ffcc26c
Christopher Pezza <chris.pezza@hey.com>
2023-03-26 21:18:10 -0400

    move navigation array to separate file

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,63 +8,7 @@ import clsx from "clsx"
 import Link from "next/link"
 import { useRouter } from "next/router"
 import { useCallback, useEffect, useState } from "react"
-
-const navigation = [
-  {
-    title: "Introduction",
-    links: [
-      { title: "Getting started", href: "/" },
-      { title: "Installation", href: "/docs/installation" },
-    ],
-  },
-  {
-    title: "Core concepts",
-    links: [
-      { title: "Understanding caching", href: "/docs/understanding-caching" },
-      {
-        title: "Predicting user behavior",
-        href: "/docs/predicting-user-behavior",
-      },
-      { title: "Basics of time-travel", href: "/docs/basics-of-time-travel" },
-      {
-        title: "Introduction to string theory",
-        href: "/docs/introduction-to-string-theory",
-      },
-      { title: "The butterfly effect", href: "/docs/the-butterfly-effect" },
-    ],
-  },
-  {
-    title: "Advanced guides",
-    links: [
-      { title: "Writing plugins", href: "/docs/writing-plugins" },
-      { title: "Neuralink integration", href: "/docs/neuralink-integration" },
-      { title: "Temporal paradoxes", href: "/docs/temporal-paradoxes" },
-      { title: "Testing", href: "/docs/testing" },
-      { title: "Compile-time caching", href: "/docs/compile-time-caching" },
-      {
-        title: "Predictive data generation",
-        href: "/docs/predictive-data-generation",
-      },
-    ],
-  },
-  {
-    title: "API reference",
-    links: [
-      { title: "CacheAdvance.predict()", href: "/docs/cacheadvance-predict" },
-      { title: "CacheAdvance.flush()", href: "/docs/cacheadvance-flush" },
-      { title: "CacheAdvance.revert()", href: "/docs/cacheadvance-revert" },
-      { title: "CacheAdvance.regret()", href: "/docs/cacheadvance-regret" },
-    ],
-  },
-  {
-    title: "Contributing",
-    links: [
-      { title: "How to contribute", href: "/docs/how-to-contribute" },
-      { title: "Architecture guide", href: "/docs/architecture-guide" },
-      { title: "Design principles", href: "/docs/design-principles" },
-    ],
-  },
-]
+import navigation from "@/data/navigation"
 
 function GitHubIcon(props) {
   return (
@@ -110,7 +54,7 @@ function Header({ navigation }) {
         <Search />
       </div>
       <div className="relative flex basis-0 justify-end gap-6 sm:gap-8 md:flex-grow">
-        <Link href="https://github.com" className="group" aria-label="GitHub">
+        <Link href="https://github.com/chiefpansancolt/simplecov-tailwindcss" className="group" aria-label="GitHub">
           <GitHubIcon className="h-6 w-6 fill-slate-400 group-hover:fill-slate-500 dark:group-hover:fill-slate-300" />
         </Link>
       </div>
---
+++ src/data/navigation.js
@@ -0,0 +1,25 @@
+const navigation = [
+  {
+    title: "Introduction",
+    links: [
+      { title: "Getting started", href: "/" },
+      { title: "Installation", href: "/docs/installation" },
+    ],
+  },
+  {
+    title: "Contributing",
+    links: [
+      { title: "How to contribute", href: "/docs/how-to-contribute" },
+      { title: "Change log", href: "/docs/change-log" },
+    ],
+  },
+  {
+    title: "Resources",
+    links: [
+      { title: "Simplecov", href: "https://github.com/simplecov-ruby/simplecov" },
+      { title: "License", href: "https://github.com/chiefpansancolt/simplecov-tailwindcss/blob/main/LICENSE" },
+    ],
+  },
+]
+
+export default navigation;
